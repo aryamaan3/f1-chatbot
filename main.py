2bf7c21d7e65770dfe09981ddc02865acd80d54d
Aryamaan Kunwar <aryamaan3@gmail.com>
2023-03-07 15:46:54 +0100

    final update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import spacy
 import datetime
 
+
 # Define a function to extract the relevant information from the JSON data
 def extract_data():
     url = "https://ergast.com/api/f1/current.json"
@@ -89,17 +90,34 @@ nlp = spacy.load("en_core_web_sm")
 
 
 def extract_fuzzy_info(text):
+    # remove punctuation
+    text = text.replace(".", "")
+    text = text.replace("?", "")
+    text = text.replace("!", "")
+    text = text.replace(",", "")
+    text = text.replace(":", "")
+    text = text.replace(";", "")
+    text = text.replace("(", "")
+    text = text.replace(")", "")
+    text = text.replace("-", " ")
+    text = text.replace("_", " ")
+    text = text.replace("/", " ")
+
     for word in text.split():
-        if word in drivers:
-            return f"Driver name: {drivers[1]} {drivers[2]}\nTeam: {drivers[3]}"
-        elif word in teams:
+        if word in teams:
             d1 = teams[word][0][1:]
             d2 = teams[word][1][1:]
             d1 = " ".join(d1)
             d2 = " ".join(d2)
             return f"Team name: {word}\nDrivers: {d1} and {d2}"
-        elif word in circuits:
-            return f"Circuit name: {circuits[1]}"
+        
+        for circuit in circuits:
+            if word in circuit:
+                return f"Circuit name: {circuit[1]}"
+            
+        for driver in drivers:
+            if word in driver:
+                return f"Driver name: {driver[1]} {driver[2]}"
 
     return "I'm sorry, I didn't understand your request. Please try again."
 
@@ -110,7 +128,13 @@ def extract_info(text):
     # Check if the user is asking for the race schedule
     words = text.lower().split()
 
-    if "schedule" in words or "race" in words or "date" in words or "time" in words or "when" in words:
+    if (
+        "schedule" in words
+        or "race" in words
+        or "date" in words
+        or "time" in words
+        or "when" in words
+    ):
         if "previous" in words and "won" not in words:
             return get_race_schedule()[1]
         elif "won" in words:
@@ -123,22 +147,20 @@ def extract_info(text):
 
     elif "standing" in words or "standings" in words or "leaderboard" in words:
         return standings()
-    
+
     elif "leading" in words or "leader" in words or "winning" in words:
         return standings()
 
     for ent in doc.ents:
-        print(ent.text, ent.label_)
-        # print(ent.text, ent.label_)
         if ent.label_ == "PERSON":
             if ent.text:
                 return get_driver_info(ent.text)
 
-        if ent.label_ in ["NORP", "GPE"]:
+        if ent.label_ in ["NORP"]:
             if ent.text:
                 return get_team_info(ent.text)
 
-        if ent.label_ in ["LOC", "FAC", "GPE", "ORG"]:
+        if ent.label_ in ["LOC", "FAC", "ORG"]:
             if ent.text:
                 return get_circuit_info(ent.text)
 
@@ -169,11 +191,16 @@ def extract_info(text):
 
     return extract_fuzzy_info(text)
 
+
 def get_last_race_winner():
-    url = 'http://ergast.com/api/f1/current/last/results.json'
+    url = "http://ergast.com/api/f1/current/last/results.json"
     response = requests.get(url)
     data = json.loads(response.text)
-    winner = data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["givenName"] + " " + data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["familyName"]
+    winner = (
+        data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["givenName"]
+        + " "
+        + data["MRData"]["RaceTable"]["Races"][0]["Results"][0]["Driver"]["familyName"]
+    )
     return winner
 
 
@@ -189,7 +216,7 @@ def get_race_schedule():
         date = race["date"]
         time = race["time"]
         location = race["Circuit"]["Location"]["country"]
-        if datetime.datetime.strptime(date, '%Y-%m-%d').date() > current_date:
+        if datetime.datetime.strptime(date, "%Y-%m-%d").date() > current_date:
             race_info += (
                 f"Round {round_number}: {race_name} on {date} at {time} in {location}\n"
             )
@@ -200,7 +227,9 @@ def get_race_schedule():
 
     # Construct the final response string
     response_next = f"The next races for the current season are:\n{race_info}"
-    response_previous = f"The previous races for the current season are:\n{race_info_previous}"
+    response_previous = (
+        f"The previous races for the current season are:\n{race_info_previous}"
+    )
     return response_next, response_previous
 
 
@@ -222,6 +251,8 @@ def get_driver_info(driver_text):
     for driver in drivers:
         if txt[1] in driver:
             return f"Driver name: {driver[1]} {driver[2]}\nNationality: {driver[3]} \nTeam: {driver[4]}"
+        
+    return f"{driver_text} is not a Formula 1 driver in 2023."
 
 
 # Define a function to generate a response to a user query for team information
@@ -231,7 +262,11 @@ def get_team_info(team_name):
     for t in txt:
         for team in teams:
             if t in team:
-                return f"Team name: {team}\nDrivers: {teams[team]}"
+                d1 = teams[team][0][1:]
+                d2 = teams[team][1][1:]
+                d1 = " ".join(d1)
+                d2 = " ".join(d2)
+                return f"Team name: {team}\nDrivers: {d1} and {d2}"
 
     return f"{team_name} is not a Formula 1 team in 2023."
 
@@ -260,8 +295,22 @@ def handle_input():
         # Extract the relevant information from the user's message
         response = extract_info(message)
         # Print the chatbot's response
-        print("Chatbot:", response,"\n")
+        print("Chatbot:", response, "\n")
 
 
 if __name__ == "__main__":
     handle_input()
+
+
+"""
+Test cases:
+"Can you give me the race schedule for the next Grand Prix?"
+"Tell me about Lewis Hamilton"
+"What team does Charles Leclerc drive for?"
+"Is there a round in Australia?"
+"Can you give me information about the Silverstone Circuit?"
+"What is the points standing for the current season?"
+"When is the next race?"
+"Is Sebastian Vettel a driver?"
+"Do you like cooking?"
+"""
